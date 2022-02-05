ace31ff8f63539a746d7c78f1f4e922268244e30
Tim Norris <tibben@ocf.berkeley.edu>
2022-02-05 18:58:12 -0500

    bg docker process flags

--- examples/docker/load-data.sh
+++ examples/docker/load-data.sh
@@ -3,7 +3,7 @@
 #
 
 #set up schema
-/usr/local/bin/docker-compose exec pg_tileserv_db sh -c "cat work/DDL/*.sql | psql -v ON_ERROR_STOP=1 --username tileserv --dbname tileserv -tA -1"
+/usr/local/bin/docker-compose exec -T pg_tileserv_db sh -c "cat work/DDL/*.sql | psql -v ON_ERROR_STOP=1 --username tileserv --dbname tileserv -tA -1"
 
 # first get data
 cd data
@@ -17,22 +17,21 @@ wget "https://s3.amazonaws.com/dmap-cache-prod/soft/8cbe4cc7-654c-4514-be00-19f7
 cd ..
 
 # Load Admin 0 countries
-/usr/local/bin/docker-compose exec pg_tileserv_db sh -c "shp2pgsql -D -s 4326 /work/ne_50m_admin_0_countries.shp | psql -U tileserv -d tileserv"
+/usr/local/bin/docker-compose exec -T pg_tileserv_db sh -c "shp2pgsql -D -s 4326 /work/ne_50m_admin_0_countries.shp | psql -U tileserv -d tileserv"
 
 # Load Vancouver Water Hydrants
-/usr/local/bin/docker-compose exec pg_tileserv_db sh -c "shp2pgsql -D -s 26910 -I /work/water-hydrants.shp hydrants | psql -U tileserv -d tileserv"
+/usr/local/bin/docker-compose exec -T pg_tileserv_db sh -c "shp2pgsql -D -s 26910 -I /work/water-hydrants.shp hydrants | psql -U tileserv -d tileserv"
 
 # Load SQL Functions for OpenLayers example
 cp ../openlayers/openlayers-function-click.sql ./data/
-/usr/local/bin/docker-compose exec pg_tileserv_db sh -c "cat /work/openlayers-function-click.sql | psql -U tileserv -d tileserv"
+/usr/local/bin/docker-compose exec -T pg_tileserv_db sh -c "cat /work/openlayers-function-click.sql | psql -U tileserv -d tileserv"
 rm ./data/openlayers-function-click.sql
 
 # Load SVI
-/usr/local/bin/docker-compose exec pg_tileserv_db sh -c "shp2pgsql -D -s 4326 /work/SVI2018_FLORIDA_tract.shp | psql -U tileserv -d tileserv"
+/usr/local/bin/docker-compose exec -T pg_tileserv_db sh -c "shp2pgsql -D -s 4326 /work/SVI2018_FLORIDA_tract.shp | psql -U tileserv -d tileserv"
 
 # load TRI
 sed -i '1 s/\(.*\),$/\1/' data/Florida_TRI_2020.csv 
-sed -i 's/(?<!\n|,)"(?!(,|$))//' data/Florida_TRI_2020.csv
 cat <<SQL | /usr/local/bin/sqlite3
 .mode csv
 .import data/Florida_TRI_2020.csv florida_tri_2020
@@ -40,6 +39,6 @@ cat <<SQL | /usr/local/bin/sqlite3
 .dump florida_tri_2020
 .quit
 SQL
-/usr/local/bin/docker-compose exec pg_tileserv_db sh -c "cat /work/Florida_TRI_2020.sql | psql -U tileserv -d tileserv"
-/usr/local/bin/docker-compose exec pg_tileserv_db sh -c "psql -U tileserv -d tileserv -c \"SELECT AddGeometryColumn ('public','florida_tri_2020','geom',4326,'POINT',2);\""
-/usr/local/bin/docker-compose exec pg_tileserv_db sh -c "psql -U tileserv -d tileserv -c \"UPDATE florida_tri_2020 SET geom=ST_SetSRID(ST_MakePoint(\\\"13. LONGITUDE\\\"::double precision,\\\"12. LATITUDE\\\"::double precision),4326) WHERE \\\"13. LONGITUDE\\\" != 'NAD83';\""
+/usr/local/bin/docker-compose exec -T pg_tileserv_db sh -c "cat /work/Florida_TRI_2020.sql | psql -U tileserv -d tileserv"
+/usr/local/bin/docker-compose exec -T pg_tileserv_db sh -c "psql -U tileserv -d tileserv -c \"SELECT AddGeometryColumn ('public','florida_tri_2020','geom',4326,'POINT',2);\""
+/usr/local/bin/docker-compose exec -T pg_tileserv_db sh -c "psql -U tileserv -d tileserv -c \"UPDATE florida_tri_2020 SET geom=ST_SetSRID(ST_MakePoint(\\\"13. LONGITUDE\\\"::double precision,\\\"12. LATITUDE\\\"::double precision),4326) WHERE \\\"13. LONGITUDE\\\" != 'NAD83';\""
