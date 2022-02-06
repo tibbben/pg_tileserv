37271415d23dc08ef0ab2d3cdc23b78cf028a6f1
EC2 Default User <ec2-user@ip-172-31-80-56.ec2.internal>
2022-02-06 22:54:30 +0000

    autoload shp csv

--- examples/docker/load-all.sh
+++ examples/docker/load-all.sh
@@ -1,11 +1,40 @@
-readarray -t collection < <(jq -c '.collection' collection.json)
+DATA_DIR=data
+readarray -t collection < <(jq -c '.collection[]' collection.json)
 for item in "${collection[@]}"
 do
         source=$(jq '.source' <<< $item)
-        readarray -t params < <(jq -r '.params | to_entries[] | "\(.key + " " + .value)"' <<< $source)
-        echo $(jq -r '.download' <<< $source) $(jq '.url' <<< $source) $(for p in "${params[@]}"; do echo  $p; done)
+        command=($(jq -r '.download' <<< $source))
+        url=$(jq '.url' <<< $source)
+
+        command+=($url)
+        filename=$(jq -r '.filename' <<< $source)
+        save='> '$DATA_DIR/$filename
+        if [ $command == "wget" ]
+        then
+                save=-'O '$DATA_DIR/$filename
+        fi
+        command+=($save)
+        echo "${command[@]}"
+        eval "${command[@]}"
         if [ $(jq -r '.format' <<< $source) == "zip" ]
         then
-                echo unzip $(jq -r '.filename' <<< $source)
+                unzip $DATA_DIR/$(jq -r '.filename' <<< $source) -d $DATA_DIR
+		rm $DATA_DIR/$filename
         fi
+done
+
+DC_DIR=/usr/local/bin
+ETL_DIR=$DATA_DIR/ETL
+DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "$DIR" ]]; then DIR="$PWD"; fi
+. "$DIR/pg.env"
+
+for item in "${collection[@]}"
+do
+        source=$(jq '.source' <<< $item)
+	filename=$(jq -r '.filename' <<< $source)
+ 	layername=$(jq -r '.layername' <<< $source)
+	load=$(jq -r '.load' <<< $source)
+	echo $DIR/$ETL_DIR/$load
+	. "$DIR/$ETL_DIR/$load"
 done
