e1ab70de25455e0827921bccf605d7ad2257d5b4
Tim Norris <tibben@ocf.berkeley.edu>
2022-02-06 18:03:59 -0500

    merge

--- examples/docker/load-all.sh
+++ examples/docker/load-all.sh
@@ -1,11 +1,22 @@
-readarray -t collection < <(jq -c '.collection' collection.json)
+DATADIR=data
+readarray -t collection < <(jq -c '.collection[]' collection.json)
 for item in "${collection[@]}"
 do
         source=$(jq '.source' <<< $item)
-        readarray -t params < <(jq -r '.params | to_entries[] | "\(.key + " " + .value)"' <<< $source)
-        echo $(jq -r '.download' <<< $source) $(jq '.url' <<< $source) $(for p in "${params[@]}"; do echo  $p; done)
+        command=($(jq -r '.download' <<< $source))
+        url=$(jq '.url' <<< $source)
+
+        command+=($url)
+        filename=$(jq -r '.filename' <<< $source)
+        save='> '$DATADIR/$filename
+        if [ $command == "wget" ]
+        then
+                save=-'O '$DATADIR/$filename
+        fi
+        command+=($save)
+        echo "${command[@]}"
         if [ $(jq -r '.format' <<< $source) == "zip" ]
         then
-                echo unzip $(jq -r '.filename' <<< $source)
+                echo unzip $DATADIR/$(jq -r '.filename' <<< $source) -d $DATADIR
         fi
 done
