15479ad7d897656c56a98be07145d58703050335
tibbben <tibben@ocf.berkeley.edu>
2022-02-07 21:11:26 +0000

    tiles

--- examples/docker/data/ETL/raster.sh
+++ examples/docker/data/ETL/raster.sh
@@ -1,21 +1,22 @@
 style=$(jq -r '.style' <<< $item)
-torender=$filename
+torender=${filename::-4}.tif
 
-if [[ " $(jq 'keys' <<<$style) " =~ "monoband-psuedo-color" ]]
+if [[ " $(jq 'keys' <<<$style) " =~ ".monobandpsuedocolor" ]]
 then
-  color_ramp=$(jq '.monoband-psuedo-color.color-ramp' <<<$style)
+  color_ramp=$(jq '.monobandpsuedocolor.colorramp' <<<$style)
   echo $color_ramp > $DATA_DIR/colors.txt
-  render=$(jq '.monoband-psuedo-color.render' <<<$style)
+  render=$(jq '.monobandpsuedocolor.render' <<<$style)
   torender=${filename::-4}_color.tif
-  $DC_DIR/docker-compose exec -T pg_tileserv_db sh -c "$render $filename $DATA_DIR/colors.txt $DATA_DIR/$torender"
-  rm $DATA_DIR/colors.txt $DATA_DIR/$filename
+  $DC_DIR/docker-compose exec -T pg_gdal sh -c "$render $filename $DATA_DIR/colors.txt $DATA_DIR/$torender"
+  rm $DATA_DIR/colors.txt $DATA_DIR/$layername.*
 fi
 
 if [[ " $(jq 'keys' <<<$style) " =~ "tiles" ]]
 then
+  [ ! -d "$DATA_DIR/tiles" ] && mkdir $DATA_DIR/tiles
   mkdir $DATA_DIR/tiles/$layername
   zoom=$(jq '.tiles.zoom' <<<$style)
-  $DC_DIR/docker-compose exec -T pg_tileserv_db sh -c "gdal2tiles.py -z $zoom --xyz $DATA_DIR/$torender $DATA_DIR/tiles/$layername"
+  $DC_DIR/docker-compose exec -T pg_gdal sh -c "gdal2tiles.py -z $zoom --xyz $DATA_DIR/$torender $DATA_DIR/tiles/$layername"
 fi
 
-rm $DATA_DIR/$torender
+rm $DATA_DIR/${torender::-4}.*
