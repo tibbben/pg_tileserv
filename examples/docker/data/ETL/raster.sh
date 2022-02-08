228346bf0676d16a9da743166a5c0c88f5d8b246
tibbben <tibben@ocf.berkeley.edu>
2022-02-08 00:19:26 +0000

    raster colors

--- examples/docker/data/ETL/raster.sh
+++ examples/docker/data/ETL/raster.sh
@@ -1,21 +1,24 @@
 style=$(jq -r '.style' <<< $item)
-torender=${filename::-4}.tif
+filename=${filename::-4}.tif
+torender=$filename
 
-if [[ " $(jq 'keys' <<<$style) " =~ ".monobandpsuedocolor" ]]
+if [[ " $(jq 'keys' <<<$style) " =~ "monobandpsuedocolor" ]]
 then
   colorramp=$(jq -r '.monobandpsuedocolor.colorramp' <<<$style)
   echo -e "$colorramp" > $DATA_DIR/colors.txt
-  render=$(jq '.monobandpsuedocolor.render' <<<$style)
+  flags=$(jq -r '.monobandpsuedocolor.flags' <<<$style)
   torender=${filename::-4}_color.tif
-  $DC_DIR/docker-compose exec -T pg_gdal sh -c "$render $filename $DATA_DIR/colors.txt $DATA_DIR/$torender"
+  sudo $DC_DIR/docker-compose exec -T pg_gdal sh -c "gdaldem color-relief $flags $DATA_DIR/$filename $DATA_DIR/colors.txt $DATA_DIR/$torender"
   rm $DATA_DIR/colors.txt $DATA_DIR/$layername.*
 fi
 
 if [[ " $(jq 'keys' <<<$style) " =~ "tiles" ]]
 then
-  mkdir $DATA_DIR/html/$layername
-  zoom=$(jq '.tiles.zoom' <<<$style)
-  $DC_DIR/docker-compose exec -T pg_gdal sh -c "gdal2tiles.py -z $zoom --xyz -w none $DATA_DIR/$torender $DATA_DIR/tiles/$layername"
+  sudo mkdir $DATA_DIR/html/$layername
+  zoom=$(jq -r '.tiles.zoom' <<<$style)
+  sudo $DC_DIR/docker-compose exec -T pg_gdal sh -c "gdal2tiles.py -z $zoom --xyz -w none $DATA_DIR/$torender $DATA_DIR/html/$layername"
+  sudo chown -R $(whoami):$(whoami) $DATA_DIR/html/$layername
+  sudo echo "*.png" > $DATA_DIR/html/$layername/.gitignore
 fi
 
 rm $DATA_DIR/${torender::-4}.*
