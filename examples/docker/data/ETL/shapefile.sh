fb98337b62b73a8c4ed118b4177cbd7b4fbb3cae
Tim Norris <tibben@ocf.berkeley.edu>
2022-02-10 11:17:04 -0500

    subset

--- examples/docker/data/ETL/shapefile.sh
+++ examples/docker/data/ETL/shapefile.sh
@@ -4,8 +4,8 @@ shapefile=/work/${filename%.*}.shp
 epsg=$(jq -r '.epsg' <<< $geography)
 sudo $DC_DIR/docker-compose exec -T pg_tileserv_db sh -c "shp2pgsql -D -s $epsg $shapefile $layername | psql -U $POSTGRES_USER -d $POSTGRES_DB"
 if [[ " $(jq 'keys' <<<$source) " =~ "subset" ]]
-do
+then
   where=$(jq -r '.subset' <<<$source)
   sudo $DC_DIR/docker-compose exec -T pg_tileserv_db sh -c "psql -U $POSTGRES_USER -d $POSTGRES_DB -c 'DELETE FROM $layername WHERE $where;'"
-done
+fi
 rm -f $DATA_DIR/${filename%.*}.*
