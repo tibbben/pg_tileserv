b58208a212646e50a529c58292359f080bedff9d
Tim Norris <tibben@ocf.berkeley.edu>
2022-02-10 22:16:34 -0500

    geology

---
+++ examples/docker/data/ETL/kml.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+kmlfile=${filename%.*}.kml
+if [[ " $(jq 'keys' <<<$source) " =~ "zipfolder" ]]
+then
+kmlfile=$(jq -r '.zipfolder' <<<$source)/${filename%.*}.kml
+fi
+if [[ " $(jq 'keys' <<<$source) " =~ "flags" ]]
+then
+  flags=$(jq -r 'keys' <<<$source) 
+fi
+echo sudo $DC_DIR/docker-compose exec -T pg_gdal sh -c "ogr2ogr $flags -f 'PostgreSQL' PG:'dbname=$POSTGRES_DB user=$POSTGRES_USER host=pg_tileserv_db password=$POSTGRES_PASSWORD' $DATA_DIR/$kmlfile"
+sudo $DC_DIR/docker-compose exec -T pg_gdal sh -c "ogr2ogr $flags -f 'PostgreSQL' PG:'dbname=$POSTGRES_DB user=$POSTGRES_USER host=pg_tileserv_db password=$POSTGRES_PASSWORD' $DATA_DIR/$kmlfile"
+# rm $DATA_DIR/$filename $DATA_DIR/$kmlfile
