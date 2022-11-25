e9a222dcdc983afe770579b7e9d2c60c9897fa61
EC2 Default User <ec2-user@ip-172-31-93-237.ec2.internal>
2022-11-25 00:48:52 +0000

    geojson flags

--- examples/docker/data/ETL/geojson.sh
+++ examples/docker/data/ETL/geojson.sh
@@ -2,7 +2,7 @@
 
 if [[ " $(jq 'keys' <<<$source) " =~ "flags" ]]
 then
-  flags=$(jq -r 'keys' <<<$source) 
+  flags=$(jq -r '.flags' <<<$source) 
 fi
 echo sudo $DC_DIR/docker-compose exec -T pg_gdal sh -c "ogr2ogr $flags -f 'PostgreSQL' PG:'dbname=$POSTGRES_DB user=$POSTGRES_USER host=pg_tileserv_db password=$POSTGRES_PASSWORD' $DATA_DIR/$filename"
 sudo $DC_DIR/docker-compose exec -T pg_gdal sh -c "ogr2ogr $flags -f 'PostgreSQL' PG:'dbname=$POSTGRES_DB user=$POSTGRES_USER host=pg_tileserv_db password=$POSTGRES_PASSWORD' $DATA_DIR/$filename"
